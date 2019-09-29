cc9757e1e201b27a2f43d9e00539822ac5fccfcf
Austin Andrews <admin@templarian.com>
2019-09-29 14:19:25 -0500

    Fancy hover for no reason.

--- src/modules/ui/navBrand/navBrand.ts
+++ src/modules/ui/navBrand/navBrand.ts
@@ -1,3 +1,22 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class NavBrand extends LightningElement { }
+export default class NavBrand extends LightningElement {
+
+  @api href: string | null = null;
+
+  handleLogoSlotChange(e: Event) {
+    const slot = this.href
+      ? this.template.childNodes[1].childNodes[0] as HTMLSlotElement
+      : this.template.childNodes[1] as HTMLSlotElement;
+    const slotElements = slot.assignedElements();
+    slotElements.forEach(element => {
+      element.dispatchEvent(new CustomEvent('slot', {
+        detail: {
+          component: 'navBrand',
+          name: 'logo',
+          variant: 'default'
+        }
+      }));
+    });
+  }
+}

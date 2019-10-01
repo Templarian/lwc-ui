9294ff231dd30d322b4b6af62860f497c73e60bd
Austin Andrews <admin@templarian.com>
2019-10-01 01:03:53 -0500

    Start fixing eslint

--- src/modules/ui/badge/badge.ts
+++ src/modules/ui/badge/badge.ts
@@ -1,3 +1,8 @@
 import { LightningElement } from 'lwc';
+import { handleSlotClass } from 'ui/util';
 
-export default class Badge extends LightningElement { }
+export default class Badge extends LightningElement {
+    connectedCallback() {
+        this.addEventListener('slot', handleSlotClass);
+    }
+}
--- src/modules/ui/util/util.ts
+++ src/modules/ui/util/util.ts
@@ -1,21 +1,42 @@
 export function updateClass(classList: DOMTokenList, classes: any) {
-  for (let [key, value] of Object.entries(classes)) {
-    if (value) {
-      classList.add(key);
-    } else {
-      classList.remove(key);
+    for (let [key, value] of Object.entries(classes)) {
+        if (value) {
+            classList.add(key);
+        } else {
+            classList.remove(key);
+        }
     }
-  }
 }
 
 export function inArrayOrDefault<T>(value: T, array: T[], defaultValue: T) {
-  return array.includes(value) ? value : defaultValue;
+    return array.includes(value) ? value : defaultValue;
 }
 
-export function updateVariant(classList: DOMTokenList, variant: string, variants: string[]) {
-  const vars = variants.reduce<object>((acc: object, v: string) => {
-    const cls = `variant-${v}`;
-    return { ...acc, [cls]: v === variant };
-  }, {});
-  updateClass(classList, vars);
+export function updateVariant(
+    classList: DOMTokenList,
+    variant: string,
+    variants: string[]
+) {
+    const vars = variants.reduce<object>((acc: object, v: string) => {
+        const cls = `variant-${v}`;
+        return { ...acc, [cls]: v === variant };
+    }, {});
+    updateClass(classList, vars);
 }
+
+interface Slot {
+    component: string;
+    name: string | null;
+    variant: string | null;
+}
+
+// Error: https://github.com/Microsoft/TypeScript/issues/28357
+export const handleSlotClass = function(event: CustomEvent<Slot>) {
+    const { target, detail: slot } = event;
+    const iconElement = target as Element;
+    const slotName = slot.name ? `-${slot.name}` : '';
+    updateClass(iconElement.classList, {
+        [`${slot.component}-variant-${slot.variant}`]: true,
+        [`${slot.component}-slot${slotName}`]: true
+    });
+} as EventListener;

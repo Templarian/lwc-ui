63635797d81e56afd9cd26d899483bbbcde90423
Austin Andrews <admin@templarian.com>
2019-10-03 01:07:54 -0500

    More examples.

--- src/modules/ui/inputCounter/inputCounter.ts
+++ src/modules/ui/inputCounter/inputCounter.ts
@@ -2,17 +2,51 @@ import { LightningElement, api, track } from 'lwc';
 import { mdiPlus, mdiMinus } from '@mdi/js';
 
 export default class InputCounter extends LightningElement {
+  @api name: string = '';
   @api value: number = 0;
   @api step: number = 1;
+  @api focus: boolean = false;
 
   @track mdiPlus: string = mdiPlus;
   @track mdiMinus: string = mdiMinus;
 
   handlePlusClick() {
     this.value += this.step;
+    this.handleChange();
   }
 
   handleMinusClick() {
     this.value -= this.step;
+    this.handleChange();
+  }
+
+  handleChange() {
+    this.dispatchEvent(
+      new CustomEvent('form_change', {
+        bubbles: true,
+        detail: {
+          name: this.name,
+          value: this.value,
+          valid: true
+        }
+      })
+    );
+  }
+
+  renderedCallback() {
+    this.dispatchEvent(
+      new CustomEvent('form_register', {
+        bubbles: true,
+        detail: {
+          name: this.name,
+          value: this.value,
+          valid: true
+        }
+      })
+    );
+    if (this.focus) {
+      const textarea = this.template.childNodes[1] as HTMLTextAreaElement;
+      textarea.focus();
+    }
   }
 }

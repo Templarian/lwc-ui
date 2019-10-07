6119ca34a74bb5c38d57fe6c506c004969105e2d
Austin Andrews <admin@templarian.com>
2019-10-06 23:33:15 -0500

    fixes.

--- src/modules/ui/navItem/navItem.ts
+++ src/modules/ui/navItem/navItem.ts
@@ -3,10 +3,7 @@ import { mdiClose } from '@mdi/js';
 import { dispatchSlot } from 'ui/util';
 
 const COMPONENT = 'navItem';
-const VARIANTS: string[] = [
-  'default',
-  'inline'
-];
+const VARIANTS: string[] = ['default', 'inline'];
 const DEFAULT_VARIANT = VARIANTS[0];
 
 export default class NavItem extends LightningElement {
@@ -18,7 +15,8 @@ export default class NavItem extends LightningElement {
   @track mdiClose: string = mdiClose;
 
   handleLeftSlotChange() {
-    const leftSlot = this.template.childNodes[1].childNodes[0] as HTMLSlotElement;
+    const leftSlot = this.template.childNodes[1]
+      .childNodes[0] as HTMLSlotElement;
     const leftSlotElements = leftSlot.assignedElements();
     leftSlotElements.forEach(element => {
       dispatchSlot(element, {
